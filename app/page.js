3eb50f03ec35ec41a94348a77eb7d5eb2e1698fa
davidalexander2411 <davidalexander2411@gmail.com>
2025-01-25 08:42:51 +0700

    sosmed

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import { IoLocationOutline } from "react-icons/io5";
 import ExperienceEducation from './components/ExperienceEducation';
 import UI from '../public/img/UI.png'
 import TechStack from './components/TechStack';
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import { AiOutlineLinkedin } from "react-icons/ai";
+
 
 
 export default function Home() {
@@ -23,11 +27,18 @@ export default function Home() {
             </h1>
           </div>
         </div>
-        <div className="text-xl inter flex grays2">
-          <h1 className="mt-1"><IoLocationOutline /></h1>
-          <h1>
-           Jakarta, Indonesia
-          </h1>
+        <div className="text-xl inter flex flex-col grays2">
+          <div className="flex flex-row">
+            <h1 className="mt-1"><IoLocationOutline /></h1>
+            <h1>
+            Jakarta, Indonesia
+            </h1>
+          </div>
+          <div className="flex flex-row text-5xl pt-3 gap-2 items-center">
+            <a href="https://www.instagram.com/david.alexander24"><FaInstagram /></a>
+            <a href="https://www.github.com/davidalexander2411"><IoLogoGithub /></a>
+            <a className="text-[52px]" href="https://www.linkedin.com/in/david-alexander-3a3601325/"><AiOutlineLinkedin/></a>
+          </div>
         </div>
         <ExperienceEducation />
         <TechStack />
